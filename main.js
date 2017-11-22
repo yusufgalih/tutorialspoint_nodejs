c67bb51da0d1f9f7472a894418b292e68f5c63d5
yusufgalih <yusuf.galih.spensa@gmail.com>
2017-11-22 11:51:58 +0700

    nambah server

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 /* Hello, World! program in node.js */
-console.log("Hello, World!")
+//console.log("Hello, World!")
+
+//-----------------------------------
+//coba server
+
+var http = require("http");
+http.createServer(function (request, response){
+    // send the Http header
+    // HTTP Status: 200 : OK
+    // Content Type : text/plain
+    response.writeHead(200, {'Content-Type' : 'text/plain'});
+
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server runnning at http://127.0.0.1:8081/');
